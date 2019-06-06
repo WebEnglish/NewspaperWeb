c5f2446f1886e611b272415ccda61399a7659108
lehung22297 <44271319+lehung22297@users.noreply.github.com>
2019-06-06 19:55:13 +0700

    Add files via upload

---
+++ DAGK/backend/model/User-model.js
@@ -0,0 +1,30 @@
+
+var db = require('../utils/Database');
+
+module.exports = {
+  all: () => {
+    return db.load('select * from thanhvien');
+  },
+
+  single: id => {
+    return db.load(`select * from thanhvien where idThanhVien = ${id}`);
+  },
+
+  singleByUserName: email => {
+    return db.load(`select * from thanhvien where Email = '${email}'`);
+  },
+
+  add: entity => {
+    return db.add('thanhvien', entity);
+  },
+
+  update: entity => {
+    var id = entity.idThanhVien;
+    delete entity.idThanhVien;
+    return db.update('thanhvien', 'idThanhVien', entity, id);
+  },
+
+  delete: id => {
+    return db.delete('thanhvien', 'idThanhVien', id);
+  }
+};
